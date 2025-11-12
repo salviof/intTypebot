d37a330ad75c83e29c587719a3334bd17ec009f5
smViana19 <samuelfilipevianadois@gmail.com>
2025-11-12 11:44:43 -0300

    criado e executado testes unitarios dos endpoints

--- src/test/java/br/org/coletivoJava/integracoes/restTypebot/implementacao/IntegracaoRestTypebotDadosResultadoTest.java
+++ src/test/java/br/org/coletivoJava/integracoes/restTypebot/implementacao/IntegracaoRestTypebotDadosResultadoTest.java
@@ -1,7 +1,27 @@
 package br.org.coletivoJava.integracoes.restTypebot.implementacao;
 
+import br.org.coletivoJava.integracoes.restTypebot.api.FabIntTypebotResultados;
+import com.super_bits.Super_Bits.mktMauticIntegracao.configAppp.ConfiguradorCoreTypebotIntegracao;
+import com.super_bits.modulosSB.SBCore.ConfigGeral.SBCore;
+import com.super_bits.modulosSB.SBCore.integracao.libRestClient.WS.conexaoWebServiceClient.ItfRespostaWebServiceSimples;
 import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class IntegracaoRestTypebotDadosResultadoTest extends TestCase {
+    public IntegracaoRestTypebotDadosResultadoTest() {
+    }
 
+    private static final String TYPEBOT_ID = "jucnz25a466rfdw2gijf8bv6";
+    private static final String RESULT_ID = "cxgiriny267lvzsukeyb4zsx";
+
+    @Test
+    public void testSomeMethod() {
+        SBCore.configurar(new ConfiguradorCoreTypebotIntegracao(), SBCore.ESTADO_APP.DESENVOLVIMENTO);
+
+        ItfRespostaWebServiceSimples resposta = FabIntTypebotResultados.DADOS_RESULTADO.getAcao(TYPEBOT_ID, RESULT_ID).getResposta();
+        System.out.println(resposta);
+        assertEquals(200, resposta.getCodigoResposta());
+    }
 }
--- src/test/java/br/org/coletivoJava/integracoes/restTypebot/implementacao/IntegracaoRestTypebotLogsResultadoTest.java
+++ src/test/java/br/org/coletivoJava/integracoes/restTypebot/implementacao/IntegracaoRestTypebotLogsResultadoTest.java
@@ -1,35 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
- */
 package br.org.coletivoJava.integracoes.restTypebot.implementacao;
 
 import br.org.coletivoJava.integracoes.restTypebot.api.FabIntTypebotResultados;
 import com.super_bits.Super_Bits.mktMauticIntegracao.configAppp.ConfiguradorCoreTypebotIntegracao;
 import com.super_bits.modulosSB.SBCore.ConfigGeral.SBCore;
 import com.super_bits.modulosSB.SBCore.integracao.libRestClient.WS.conexaoWebServiceClient.ItfRespostaWebServiceSimples;
+import junit.framework.TestCase;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
-/**
- *
- * @author salvio
- */
-public class IntegracaoRestTypebotListar7DiasTest {
+import static org.junit.Assert.assertEquals;
 
-    public IntegracaoRestTypebotListar7DiasTest() {
+public class IntegracaoRestTypebotLogsResultadoTest {
+    public IntegracaoRestTypebotLogsResultadoTest() {
     }
 
     private static final String TYPEBOT_ID = "jucnz25a466rfdw2gijf8bv6";
-    private static final int LIMIT = 50;
+    private static final String RESULT_ID = "cxgiriny267lvzsukeyb4zsx";
 
     @Test
     public void testSomeMethod() {
         SBCore.configurar(new ConfiguradorCoreTypebotIntegracao(), SBCore.ESTADO_APP.DESENVOLVIMENTO);
 
-        ItfRespostaWebServiceSimples resposta = FabIntTypebotResultados.LISTAR_7_DIAS.getAcao(TYPEBOT_ID, LIMIT).getResposta();
+        ItfRespostaWebServiceSimples resposta = FabIntTypebotResultados.LOGS_RESULTADO.getAcao(TYPEBOT_ID, RESULT_ID).getResposta();
         System.out.println(resposta);
         assertEquals(200, resposta.getCodigoResposta());
     }
-
 }
